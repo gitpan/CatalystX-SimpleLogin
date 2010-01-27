0e1c3375ad1682e7ab8b950882c845dcc2a3fcf3
Tomas Doran <bobtfish@bobtfish.net>
2010-01-27 22:02:36 +0000

    Import of BOBTFISH/CatalystX-SimpleLogin-0.08 from CPAN.
    gitpan-cpan-distribution: CatalystX-SimpleLogin
    gitpan-cpan-version:      0.08
    gitpan-cpan-path:         BOBTFISH/CatalystX-SimpleLogin-0.08.tar.gz
    gitpan-cpan-author:       BOBTFISH
    gitpan-cpan-maturity:     released

--- lib/CatalystX/SimpleLogin/TraitFor/Controller/Login/WithRedirect.pm
+++ lib/CatalystX/SimpleLogin/TraitFor/Controller/Login/WithRedirect.pm
@@ -14,17 +14,15 @@ around 'redirect_after_login_uri' => sub {
         return $self->$orig($c, @args);
     }
     return $c->session->{redirect_to_after_login}
-        ? $c->session->{redirect_to_after_login}
+        ? delete $c->session->{redirect_to_after_login}
         : $self->$orig($c, @args);
 };
 
-sub login_redirect {
+before login_redirect => sub {
     my ($self, $c, $message) = @_;
     $c->flash->{error_msg} = $message; # FIXME - Flash horrible
     $c->session->{redirect_to_after_login} = $c->uri_for($c->action, $c->req->captures, $c->req->args->flatten, $c->req->parameters);
-    $c->response->redirect($c->uri_for($self->action_for("login")));
-    $c->detach;
-}
+};
 
 1;
 
